1c0909a7b26eddcf2642ba04e28446d0889b81e4
Michael Sickles <michael.c.sickles@gmail.com>
2021-06-11 08:55:54 -0400

    Skip test and 1 less install

--- run.sh
+++ run.sh
@@ -16,15 +16,14 @@ fi
 build() {
   cd producer || exit
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  mvn install
+  mvn install -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   java -javaagent:/opt/agents/otel.jar -Dotel.resource.attributes=service.name=producer -Dconsumer_port="${CONSUMER_PORT}"  -jar target/*.jar --server.port=${PRODUCER_PORT} &
   cd ../
   cd consumer || exit
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  mvn install
+  mvn install -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  mvn install
   java -javaagent:/opt/agents/otel.jar -Dotel.resource.attributes=service.name=consumer -jar target/*.jar --server.port=${CONSUMER_PORT} &
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Run Complete-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   
