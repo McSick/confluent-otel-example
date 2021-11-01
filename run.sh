d0a5108aa836cd37fed62183dd2482a637a5b8ac
Michael Sickles <michael.c.sickles@gmail.com>
2021-11-01 12:44:13 -0400

    add otel jar

--- run.sh
+++ run.sh
@@ -18,13 +18,13 @@ build() {
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   ./mvnw package -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  java -javaagent:~/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=producer -Dconsumer_port="${CONSUMER_PORT}"  -jar target/*.jar --server.port=${PRODUCER_PORT} &
+  java -javaagent:../lib/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=producer -Dconsumer_port="${CONSUMER_PORT}"  -jar target/*.jar --server.port=${PRODUCER_PORT} &
   cd ../
   cd consumer || exit
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   ./mvnw package -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  java -javaagent:~/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=consumer -jar target/*.jar --server.port=${CONSUMER_PORT} &
+  java -javaagent:../lib/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=consumer -jar target/*.jar --server.port=${CONSUMER_PORT} &
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Run Complete-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   
 }
