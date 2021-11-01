c15810c1a3972a2d554025748fa403876ff7d66d
Michael Sickles <michael.c.sickles@gmail.com>
2021-11-01 12:15:18 -0400

    maven

--- run.sh
+++ run.sh
@@ -16,13 +16,13 @@ fi
 build() {
   cd producer || exit
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  mvn install -DskipTests
+  ./mvnw package -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Producer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   java -javaagent:~/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=producer -Dconsumer_port="${CONSUMER_PORT}"  -jar target/*.jar --server.port=${PRODUCER_PORT} &
   cd ../
   cd consumer || exit
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Installing Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-  mvn install -DskipTests
+  ./mvnw package -DskipTests
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Starting Consumer-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   java -javaagent:~/opentelemetry-javaagent-all.jar -Dotel.resource.attributes=service.name=consumer -jar target/*.jar --server.port=${CONSUMER_PORT} &
   echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Run Complete-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
